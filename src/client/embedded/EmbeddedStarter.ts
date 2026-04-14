2e71c1ec61736f014f08c8eeda3822f047baaee1
Valentin Herrmann <accounts@valentin-herrmann.com>
2026-04-14 17:50:14 +0200

    feat: update Monaco worker imports to use inline workers for improved performance
    Signed-off-by: Valentin Herrmann <accounts@valentin-herrmann.com>

--- src/client/embedded/EmbeddedStarter.ts
+++ src/client/embedded/EmbeddedStarter.ts
@@ -4,11 +4,11 @@ import * as PIXI from 'pixi.js';
 import { ThemeManager } from "../main/gui/ThemeManager.js";
 import { MainEmbedded } from "./MainEmbedded.js";
 
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker&inline'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker&inline'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker&inline'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline'
 
 
 // All css files for embedded online-ide:
