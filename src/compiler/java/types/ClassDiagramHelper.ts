9d42b09695e071f5485e1e532328c01f5a417147
Martin Pabst <asv.pabst@gmail.com>
2026-05-20 10:56:06 +0200

    bugfix: class diagram didn't display aggregations with type ArrayList<OtherType>

--- src/compiler/java/types/ClassDiagramHelper.ts
+++ src/compiler/java/types/ClassDiagramHelper.ts
@@ -10,18 +10,18 @@ export type CompostionData = { klass: IJavaClass | IJavaInterface, identifier: s
 
 export class ClassDiagramHelper {
     registerUsedSystemClasses(klass: JavaClass, usedSystemClasses: Set<IJavaClass | IJavaInterface>) {
-        if(klass.getExtends() && (klass.getExtends()?.module instanceof SystemModule)){
+        if (klass.getExtends() && (klass.getExtends()?.module instanceof SystemModule)) {
             usedSystemClasses.add(this.getTypeWithoutGenerics(klass.getExtends()!));
         }
 
-        for(let intf of klass.getImplements()){
-            if(intf.module instanceof SystemModule){
+        for (let intf of klass.getImplements()) {
+            if (intf.module instanceof SystemModule) {
                 usedSystemClasses.add(this.getTypeWithoutGenerics(intf));
             }
         }
 
-        for(let cd of this.getCompositeData(klass)){
-            if(cd.klass && cd.klass.module instanceof SystemModule){
+        for (let cd of this.getCompositeData(klass)) {
+            if (cd.klass && cd.klass.module instanceof SystemModule) {
                 usedSystemClasses.add(cd.klass);
             }
         }
@@ -30,9 +30,9 @@ export class ClassDiagramHelper {
 
 
     getTypeWithoutGenerics(type: IJavaClass | IJavaInterface): JavaClass | JavaInterface {
-        if(type instanceof GenericVariantOfJavaClass) return type.isGenericVariantOf;
-        if(type instanceof GenericVariantOfJavaInterface) return type.isGenericVariantOf;
-        return <JavaClass | JavaInterface> type;
+        if (type instanceof GenericVariantOfJavaClass) return type.isGenericVariantOf;
+        if (type instanceof GenericVariantOfJavaInterface) return type.isGenericVariantOf;
+        return <JavaClass | JavaInterface>type;
     }
 
     aggregatingClassIdentifiers: string[] = ["list", "set", "menge", "tree", "baum"];
@@ -45,21 +45,23 @@ export class ClassDiagramHelper {
             let type: JavaType | undefined = a.type;
             if (type && (a.type instanceof IJavaClass) || (a.type instanceof IJavaInterface)
                 || (a.type instanceof JavaArrayType)) {
-                
+
                 let isAggregatingClass = this.aggregatingClassIdentifiers.some(id => a.type?.identifier.toLowerCase().includes(id));
 
                 if (isAggregatingClass &&
-                type instanceof GenericVariantOfJavaClass && type.typeMap?.size == 1) {
+                    type instanceof GenericVariantOfJavaClass) {
                     type.typeMap.forEach((v, k) => {
-                        type = v;
+                        if (v instanceof IJavaClass || v instanceof IJavaInterface) {
+                            type = v;
+                        }
                     });
                 }
 
-                while(type instanceof JavaArrayType){
+                while (type instanceof JavaArrayType) {
                     type = type.elementType;
                 }
 
-                if(!type) continue;
+                if (!type) continue;
 
                 let cda = cdMap.get(a.type);
                 if (cda == null) {
