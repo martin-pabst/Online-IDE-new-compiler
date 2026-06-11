374d35841fc9bba400d9a714a13bd5f11d91840b
Martin Pabst <asv.pabst@gmail.com>
2026-06-11 16:48:13 +0200

    new .assert-syntax in asm testfile

--- src/test/assembly/LoadStoreTest.asm
+++ src/test/assembly/LoadStoreTest.asm
@@ -6,6 +6,6 @@ store 10
 load 10
 addi 20
 store 11
-.assert 10 101, 120, "Error performing load/store operations"
+.assert {10:[100, 120], message: "Error performing load/store operations"}
 hold
 
