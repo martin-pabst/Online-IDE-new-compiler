338dd3d688d9daf95e00ea23c54054cceab1628a
Martin Pabst <asv.pabst@gmail.com>
2026-06-11 11:25:11 +0200

    hide assembly language

--- src/compiler/common/programminglanguage/ProgrammingLanguageManager.ts
+++ src/compiler/common/programminglanguage/ProgrammingLanguageManager.ts
@@ -11,7 +11,7 @@ export class ProgrammingLanguageManager {
     private constructor(){
         ProgrammingLanguageManager.instance = this;
         this.languages.push(JavaLanguage.getInstance());
-        this.languages.push(AssemblyLanguage.getInstance());
+        // this.languages.push(AssemblyLanguage.getInstance());
     }
 
     public static getInstance(): ProgrammingLanguageManager {
